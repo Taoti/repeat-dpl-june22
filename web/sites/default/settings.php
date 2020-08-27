db17c752c2491fb94c5ed592e83e90127bbb6a9f
Nick Dickinson-Wilde <nick@nickdickinsonwilde.ca>
2020-08-27 06:47:45 -0700

    Support old MariaDB versions

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -9,7 +9,7 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  * Include the Pantheon-specific settings file.
  *
  * n.b. The settings.pantheon.php file makes some changes
- *      that affect all envrionments that this site
+ *      that affect all environments that this site
  *      exists in.  Always include this file, even in
  *      a local development environment, to ensure that
  *      the site settings remain consistent.
@@ -20,6 +20,11 @@ include __DIR__ . "/settings.pantheon.php";
  * Place the config directory outside of the Drupal root.
  */
 $settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
+
+/**
+ * Support MariaDB 10.0 - needed on Pantheon for Drupal 9 until they update.
+ */
+$databases['default']['default']['namespace'] = 'Drupal\\Driver\\Database\\mysql';
 
 /**
  * If there is a local settings file, then include it
